f955dc31e7417ebe03840e7e8b831a2bcf452f9c
AIvars Lauzis <lauzis@inbox.lv>
2018-09-05 09:00:10 +0300

    - starting implement tracking by link destination

--- assets/js/parts/gae-track-links-to-specific-urls.js
+++ assets/js/parts/gae-track-links-to-specific-urls.js
@@ -1,1 +1,13 @@
 debug_message("todo track links to specific urls");
+var gae_destinations_to_track = "[gae-track-links-to-specific-urls-list]";
+console.log(gae_destinations_to_track);
+
+gae_destinations_to_track = gae_destinations_to_track.split(",");
+console.log(gae_destinations_to_track);
+
+var gaeD2T = null;
+for(gaeD2T  in gae_destinations_to_track){
+
+    console.log(gae_destinations_to_track[gaeD2T]);
+
+}
