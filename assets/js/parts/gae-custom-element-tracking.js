363a8faf921ebc89305c23b48e015e2f3fad24af
AIvars Lauzis <lauzis@inbox.lv>
2018-09-06 09:12:52 +0300

    - custome element tracking by class implemented

--- assets/js/parts/gae-custom-element-tracking.js
+++ assets/js/parts/gae-custom-element-tracking.js
@@ -1,34 +1,39 @@
-$(click_tracking_elements).each(function(){
+var track_elements ='[gae-custom-element-tracking-class]';
 
-  var self = $(this);
+if (track_elements.length){
 
-  if (!self.hasClass("gae-events")){
-    self.on("click",function(){
+    $(track_elements).each(function(){
 
         var self = $(this);
-        var category = self.data("gaCategory");
-        var action = self.data("gaAction");
-        var label = self.data("gaLabel");
-        var value = self.data("gaValue");
-        if (!category){
-            category = self.prop("tagName");
-        }
-        if (!action){
-            action = "Clicked";
-        }
-        if (!label){
-            label = get_link_text(self);
-        }
-        if (!value){
-            value = null;
+
+        if (!self.hasClass("gae-events")){
+            self.on("click",function(){
+
+                var self = $(this);
+                var category = self.data("gaCategory");
+                var action = self.data("gaAction");
+                var label = self.data("gaLabel");
+                var value = self.data("gaValue");
+                if (!category){
+                    category = self.prop("tagName");
+                }
+                if (!action){
+                    action = "Clicked";
+                }
+                if (!label){
+                    label = get_link_text(self);
+                }
+                if (!value){
+                    value = null;
+                }
+
+                send_event(category, action, label, value)
+            });
+            self.addClass("gae-events");
+            self.addClass("gae-events-custom-element-tracking");
         }
 
-        send_event(category, action, label, value)
     });
-    self.addClass("gae-events");
-  }
-
-});
-
+}
 
 debug_message("Assigned click events to custom elements");
