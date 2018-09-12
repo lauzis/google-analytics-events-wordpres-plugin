fe40834a0f6bc4949618273fcf3b718a71d0d5cb
AIvars Lauzis <lauzis@inbox.lv>
2018-09-12 23:16:12 +0300

    removed un used code

--- assets/js/parts/gae-variables.js
+++ assets/js/parts/gae-variables.js
@@ -29,14 +29,6 @@ if (typeof(GAE_TIME_TRIGGER_TRESHOLD)==="undefined"){
     var GAE_TIME_TRIGGER_TRESHOLD = [gae-time-trigger-treshold];
 }
 
-if (typeof contact_page_link === "undefined"){
-    var contact_page_link="";
-}
-
-if (typeof lang === "undefined"){
-    var lang="en";
-}
-
 var tracked_form_values = [];
 
 //gravity forms class for tracking value and sending it to when form is succesfull
