a44aeaab432c9afcf5b569a25229d0698ecffe76
AIvars Lauzis <lauzis@inbox.lv>
2018-10-04 00:28:31 +0300

    settings moved tot the top

--- assets/js/parts/gae-variables.js
+++ assets/js/parts/gae-variables.js
@@ -1,42 +1,3 @@
-if (typeof GAE_DEBUG_LEVEL === "undefined"){
-    var GAE_DEBUG_LEVEL = [gae-debug-level];
-}
-
-if (typeof GAE_SCRIPT_TYPE === "undefined"){
-    var GAE_SCRIPT_TYPE = [gae-script-type];
-
-    if (GAE_SCRIPT_TYPE===-1){
-        debug_message("Will try to detect if analytics is included or not!");
-        function_name="";
-        if (typeof ga === "function"){
-            GAE_SCRIPT_TYPE = 1;
-            function_name="ga";
-        }
-        if (typeof gtag === "function"){
-            GAE_SCRIPT_TYPE = 0;
-            function_name="gtag";
-        }
-        //if still unknow test failed
-        if (GAE_SCRIPT_TYPE===-1){
-            debug_message("We could not find the gtag/ga, must be that google analytics script is not included!");
-        } else {
-            debug_message("We found what we, need. Analytics "+function_name+"() detected!");
-        }
-    }
-}
-
-if (typeof(GAE_TIME_TRIGGER_THRESHOLD)==="undefined"){
-    var GAE_TIME_TRIGGER_THRESHOLD = [gae-time-trigger-threshold];
-}
-
-var tracked_form_values = [];
-
-//gravity forms class for tracking value and sending it to when form is succesfull
-var value_tracking_selector = '.ga-track-value select, .ga-track-value input, select.ga-track-value, input.ga-track-value';
-
-var HOST = document.location.hostname;
-
-
 const GAE_SECTIONS = [
     {
         id: 'gae-event-contact-links',
@@ -99,3 +60,41 @@ const GAE_SECTIONS = [
         enabled: [gae-event-track-links-to-specific-urls]
     }
 ];
+
+if (typeof GAE_DEBUG_LEVEL === "undefined"){
+    var GAE_DEBUG_LEVEL = [gae-debug-level];
+}
+
+if (typeof GAE_SCRIPT_TYPE === "undefined"){
+    var GAE_SCRIPT_TYPE = [gae-script-type];
+
+    if (GAE_SCRIPT_TYPE===-1){
+        debug_message("Will try to detect if analytics is included or not!");
+        function_name="";
+        if (typeof ga === "function"){
+            GAE_SCRIPT_TYPE = 1;
+            function_name="ga";
+        }
+        if (typeof gtag === "function"){
+            GAE_SCRIPT_TYPE = 0;
+            function_name="gtag";
+        }
+        //if still unknow test failed
+        if (GAE_SCRIPT_TYPE===-1){
+            debug_message("We could not find the gtag/ga, must be that google analytics script is not included!");
+        } else {
+            debug_message("We found what we, need. Analytics "+function_name+"() detected!");
+        }
+    }
+}
+
+if (typeof(GAE_TIME_TRIGGER_THRESHOLD)==="undefined"){
+    var GAE_TIME_TRIGGER_THRESHOLD = [gae-time-trigger-threshold];
+}
+
+var tracked_form_values = [];
+
+//gravity forms class for tracking value and sending it to when form is succesfull
+var value_tracking_selector = '.ga-track-value select, .ga-track-value input, select.ga-track-value, input.ga-track-value';
+
+var HOST = document.location.hostname;
