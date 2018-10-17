b181ff16e7b32a5290c5039e26f53b215d4c1b1d
AIvars Lauzis <lauzis@inbox.lv>
2018-10-17 07:59:34 +0300

    - admin styling, colapsed, and show by collor to see easely what sections are on off - introduced swithc, to esier find what elements are switched on of - removed some empty lines - added inline script for the collapsing, showwing the sections - spaceing and color changes in admin - last section opened by default

--- gae_settings_page.php
+++ gae_settings_page.php
@@ -1,63 +1,106 @@
 <div class="<?= gae_PLUGIN_DIRECTORY ?>">
-  <?php Gae_Admin::generate_combined(); ?>
-  <?php $sections = Gae_Admin::get_sections(); ?>
-  <h1><?php _e('Google analytics settings',gae_PUGIN_NAME); ?> - <?php print gae_PUGIN_NAME ." ". gae_CURRENT_VERSION; ?></h1>
-  <?php Gae_Admin::print_all_messages(); ?>
-  <p>
-      To setup goals for you website, you have to collect events on the page. Usually events are added via google tag manager, or hardcoded in the page.
-      This plugin sets some basic events, that should be collected, also possible to add some custom events, for your custome elements.
-      Bellow there is several sections that can be enabled or disabled separately.
-      More about event and event tracking read <a href="https://wpflow.com/what-is-google-analytics-event-tracking/" target="_blank">here</a>.
-  </p>
+    <?php Gae_Admin::generate_combined(); ?>
+    <?php $sections = Gae_Admin::get_sections(); ?>
+    <h1><?php _e('Google analytics settings', gae_PUGIN_NAME); ?>
+        - <?php print gae_PUGIN_NAME . " " . gae_CURRENT_VERSION; ?></h1>
+    <?php Gae_Admin::print_all_messages(); ?>
+    <p>
+        To setup goals for you website, you have to collect events on the page. Usually events are added via google tag
+        manager, or hardcoded in the page.
+        This plugin sets some basic events, that should be collected, also possible to add some custom events, for your
+        custome elements.
+        Bellow there is several sections that can be enabled or disabled separately.
+        More about event and event tracking read <a href="https://wpflow.com/what-is-google-analytics-event-tracking/"
+                                                    target="_blank">here</a>.
+    </p>
 
 
+    <?php if (Gae_Admin::show_donation_block()) : ?>
+        <?php include(gae_INCLUDES_PATH . "/donation.php"); ?>
+    <?php endif; ?>
 
+    <?php
+        $enabled_values = [
+            1,
+            "tag-manager",
+            "analytics",
+            "enable-php-log",
+            "enable-console-log",
+            "enable-show-on-front"
+        ];
 
-  <?php if (Gae_Admin::show_donation_block()) : ?>
-  <?php include(gae_INCLUDES_PATH."/donation.php"); ?>
-  <?php endif; ?>
+        $disabled_values = [
+            0,
+            "added-tag-manager",
+            "added-tag-analytics",
+            "added-idk",
+            "disabled"
+        ]
 
-  <form method="post" action="" autocomplete="off">
-    <?php settings_fields( 'gae-settings-group' ); ?>
-    <?php foreach($sections as $section): ?>
-        <section id="<?= $section["id"] ?>" class="<?= gae_PLUGIN_DIRECTORY ?>-section">
+    ?>
 
-            <h2 id="section-<?= $section["id"] ?>"><?= $section["title"]; ?></h2>
-            <p class="<?= gae_PLUGIN_DIRECTORY ?>-description">
-                <?= $section["description"]; ?>
+    <form method="post" action="" autocomplete="off">
+        <?php settings_fields('gae-settings-group'); ?>
+        <?php foreach ($sections as $section): ?>
+            <?php $enabled=""; ?>
+            <?php foreach($section["fields"] as $field){
 
-            </p>
-            <?php if (!empty($section["example"])): ?>
-            
-              <code class="<?= gae_PLUGIN_DIRECTORY ?>-code">
-                
-                  <?= htmlentities($section["example"]); ?>
-                
-              </code>
-            
-            <?php endif; ?>
-
-            <ul id="section-<?= $section["id"] ?>-content" class="<?= gae_PLUGIN_DIRECTORY ?>-content">
-              <?php foreach($section["fields"] as $field): ?>
-                <?php $title=$field["title"] ?>
-                <?php $id=$field["id"] ?>
-                <?php $value=$field["value"] ?>
-                <?php $default_value=$field["default_value"] ?>
-                <?php $placeholder= !empty($field["placeholder"]) ? $field["placeholder"] : "" ?>
-                <?php $options= !empty($field["options"]) ? $field["options"] : [] ?>
-                <?php $description= !empty($field["description"]) ? $field["description"] : "" ?>
-                <?php if ($id==="gea-debug-ip"){
-                    $description.="<br/>You current ip address is: ".$_SERVER["REMOTE_ADDR"];
+                if (in_array($field["value"],$enabled_values) && ($field["type"]=="switch" || $field["type"]=="select")){
+                    $enabled=" section-enabled";
+                    break;
+                } elseif (in_array($field["value"],$disabled_values) && ($field["type"]=="switch" || $field["type"]=="select")) {
+                    $enabled=" section-disabled";
                 }
-                ?>
-                <li><?php require(gae_INCLUDES_PATH."/fields/".$field["type"].".php"); ?></li>
-              <?php endforeach; ?>
-            </ul>
+            } ?>
+            <section id="<?= $section["id"] ?>" class="<?= gae_PLUGIN_DIRECTORY ?>-section<?= $enabled ?>">
 
+                <h2 id="section-<?= $section["id"] ?>" class="section-title"><?= $section["title"]; ?></h2>
+                <p class="<?= gae_PLUGIN_DIRECTORY ?>-description">
+                    <?= $section["description"]; ?>
+                </p>
+                <?php if (!empty($section["example"])): ?>
+                    <code class="<?= gae_PLUGIN_DIRECTORY ?>-code">
+                        <?= htmlentities($section["example"]); ?>
+                    </code>
+                <?php endif; ?>
+
+                <ul id="section-<?= $section["id"] ?>-content" class="<?= gae_PLUGIN_DIRECTORY ?>-content">
+                    <?php foreach ($section["fields"] as $field): ?>
+                        <?php $title = $field["title"] ?>
+                        <?php $id = $field["id"] ?>
+                        <?php $value = $field["value"] ?>
+                        <?php $default_value = $field["default_value"] ?>
+                        <?php $placeholder = !empty($field["placeholder"]) ? $field["placeholder"] : "" ?>
+                        <?php $options = !empty($field["options"]) ? $field["options"] : [] ?>
+                        <?php $description = !empty($field["description"]) ? $field["description"] : "" ?>
+                        <?php if ($id === "gea-debug-ip") {
+                            $description .= "<br/>You current ip address is: " . $_SERVER["REMOTE_ADDR"];
+                        }
+                        ?>
+                        <li><?php require(gae_INCLUDES_PATH . "/fields/" . $field["type"] . ".php"); ?></li>
+                    <?php endforeach; ?>
+                </ul>
+
+            </section>
+        <?php endforeach; ?>
+        <section class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>"/>
         </section>
-    <?php endforeach; ?>
-    <section class="submit">
-      <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-    </section>
-  </form>
+    </form>
+
+    <script>
+        jQuery(document).ready(function(){
+            jQuery('.section-title').click(function(){
+                console.log("clieck");
+                var self =  jQuery(this).parent();
+                if (self.hasClass("section-open")){
+                    self.removeClass("section-open");
+                } else {
+                    self.addClass("section-open");
+                }
+            });
+
+            jQuery('.google-analytics-events-section').last().addClass("section-open");
+        });
+    </script>
 </div>
---
+++ inc/fields/switch.php
@@ -0,0 +1,20 @@
+<div class="gae-form-field" id="gae-form-field-<?= $id ?>">
+
+  <label for="<?= $id ?>">
+    <?= $title ?>
+  </label>
+
+  <select name="<?= $id ?>" id="<?= $id ?>">
+    <?php foreach($options as $o): ?>
+      <option
+      <?php if ((strlen($value)>0 && $value===$o["value"]) || (empty($value) && $default_value===$o["value"])): ?>
+        selected="selected"
+      <?php endif; ?>
+      value="<?= $o["value"] ?>"><?= $o["title"] ?></option>
+    <?php endforeach; ?>
+  </select>
+  <?php if ($description): ?>
+    <p class="gae-form-field-description"><?= $description ?></p>
+  <?php endif; ?>
+
+</div>
