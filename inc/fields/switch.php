075172fd2859764ab0298e6f6fbf174a39ddeb18
AIvars Lauzis <lauzis@inbox.lv>
2018-11-01 08:56:55 +0200

    - added wrapper for secets - some stiling for amdin side adjusted

--- inc/fields/switch.php
+++ inc/fields/switch.php
@@ -1,20 +1,23 @@
-<div class="gae-form-field" id="gae-form-field-<?= $id ?>">
+<div class="gae-form-field gae-form-field-select" id="gae-form-field-<?= $id ?>">
 
-  <label for="<?= $id ?>">
-    <?= $title ?>
-  </label>
+    <label for="<?= $id ?>">
+        <?= $title ?>
+    </label>
 
-  <select name="<?= $id ?>" id="<?= $id ?>">
-    <?php foreach($options as $o): ?>
-      <option
-      <?php if ((strlen($value)>0 && $value===$o["value"]) || (empty($value) && $default_value===$o["value"])): ?>
-        selected="selected"
-      <?php endif; ?>
-      value="<?= $o["value"] ?>"><?= $o["title"] ?></option>
-    <?php endforeach; ?>
-  </select>
-  <?php if ($description): ?>
-    <p class="gae-form-field-description"><?= $description ?></p>
-  <?php endif; ?>
+    <div class="gae-form-field-select-wrap">
+        <select name="<?= $id ?>" id="<?= $id ?>">
+            <?php foreach($options as $o): ?>
+                <option
+                    <?php if ((strlen($value)>0 && $value===$o["value"]) || (empty($value) && $default_value===$o["value"])): ?>
+                        selected="selected"
+                    <?php endif; ?>
+                        value="<?= $o["value"] ?>"><?= $o["title"] ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+
+    <?php if ($description): ?>
+        <p class="gae-form-field-description"><?= $description ?></p>
+    <?php endif; ?>
 
 </div>
